ce2f4829c44a0553fc36654f190edcfef2ccf41e
J.R. Phillips <jr@pipefl.com>
2018-11-27 15:56:06 -0700

    New items

---
+++ Functions/Get-RubrikEvent.ps1
@@ -0,0 +1,59 @@
+function Get-RubrikEvent {
+
+    param(
+        [parameter()]
+        [int]$limit = 50,
+        [parameter()]
+        [string]$after_id,
+        [parameter()]
+        [string]$event_series_id,
+        [parameter()]
+        [string]$status,
+        [parameter()]
+        [string]$event_type,
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string]$object_ids,
+        [parameter()]
+        [string]$object_name,
+        [parameter()]
+        [string]$before_date,
+        [parameter()]
+        [string]$after_date,
+        [parameter()]
+        [string]$object_type,
+        [parameter()]
+        [bool]$show_only_latest = $true
+    )
+    <#
+    .SYNOPSIS
+    Powershell Function to search for rubrik events. 
+     
+    .EXAMPLE
+    Get-RubrikEvent -event_type Failure -object_name SQL01 -after_date (get-date).adddays(-1)
+    #>
+
+    if ($before_date) {$before_date = get-date $before_date -UFormat "%a %b %d %T %Y"}
+    if ($after_date) {$after_date = get-date $after_date -UFormat "%a %b %d %T %Y"}
+
+    $endpointURI = 'event?'
+    $plist = $PSBoundParameters
+    if ($PSBoundParameters) {
+        foreach ($i in $plist.keys) {
+            $par = $i
+            $val = $plist[$i]
+            $endpointURI = $endpointURI + $par + '=' + $val + '&'
+        }
+    }
+
+    $endpointURI = $endpointURI.Replace(' ','%20')
+    $endpointURI = $endpointURI.Replace(',','%2C')
+    $endpointURI = $endpointURI.Replace(':','%3A')
+
+    if ($endpointURI[-1] -eq '&') {$endpointURI = $endpointURI.Substring(0,$endpointURI.Length-1)}
+    $endpointURI 
+    $jobdata = Invoke-RubrikRESTCall -Endpoint $endpointURI -Method GET -api internal -verbose
+    $jobdata.data
+}
+
+
+
---
+++ Scripts/Restore-RubrikSQLDatabaseV5.ps1
@@ -0,0 +1,104 @@
+﻿param(
+    [parameter(mandatory)]
+    [string] $dbserver,
+    [parameter(mandatory)]
+    [string] $database,
+    [parameter(mandatory)]
+    [string] $targetdbserver,
+    [parameter()]
+    [string] $instance = "MSSQLSERVER",
+    [parameter()]
+    [string] $targetdbname = $database,
+    [parameter(mandatory)]
+    [string] $rubrik,
+    [parameter()]
+    [string] $dbfilepath,
+    [parameter()]
+    [string] $logfilepath,
+	[Parameter()]
+    [switch] $uselatestsnapshot,
+    [Parameter()]
+    [switch] $usescriptcredentials
+)
+
+if ($dbfilepath -and !$logfilepath) { 
+    write-host -ForegroundColor yellow Please spcify both -dbfilepath and -logfilepath
+    exit}
+
+<# 
+A function here for adding a login to the script. You can actually construct the credential opbject however you like. 
+1. Log into the system that is running this script as the service account that execute the script. 
+2. Create the rubrik credential object by typing: 
+    $creds = get-credential 
+    and then enter the rubrik username and password.
+3. Store that credential as a secure XML by typing: 
+    $creds | Export-CliXML -path .\credential.xml -force
+#>
+
+function runrubriklogin {
+    param(
+        [parameter(mandatory)]
+        [String] $rbk
+    )
+        $creds = Import-Clixml -Path .\credential.xml 
+        Connect-Rubrik -Server $rbk -Credential $creds 
+}
+
+if ($usescriptcredentials) {
+    runrubriklogin -rbk $rubrik
+}
+
+# Validate SQL server and refresh metadata
+
+$targetdbserver = (Get-RubrikHost -name $targetdbserver | where-object {$_.hostname -eq $targetdbserver}).hostname
+
+# replace below with code that I'll send you later to refresh the host. 
+
+$targetdbserverid = (Get-RubrikHost -name $targetdbserver | where-object {$_.hostname -eq $targetdbserver}).id
+$endpointURI = 'host/' + $targetdbserverid + '/refresh'
+Invoke-RubrikRESTCall -Endpoint $endpointURI -Method POST
+
+# Validate and grab the Database
+
+try {
+    $rdb = Get-RubrikDatabase -Hostname $dbserver -Instance $instance -name $database | Get-RubrikDatabase -ea silentlycontinue}
+catch {
+    write-host -ForegroundColor Red Could not acquire $database on $dbserver
+    Exit}
+
+$SourceDBID = $rdb.id
+
+if ($uselatestsnapshot) {
+	$latestsnapdate = (Get-RubrikDatabase -Hostname $dbserver -Database $database | Get-RubrikSnapshot | Sort-Object date)[-1].date
+	$recoverdate = Get-Date $latestsnapdate 
+} else {
+	$recoverdate = $rdb.latestRecoveryPoint | get-date
+}
+
+try {
+    $TargetInstanceID = (Get-RubrikSQLInstance -Name $targetinstance -Hostname $targetdbserver).id}
+catch {
+    write-host -ForegroundColor red Could not acquire $targetdbserver information
+    exit}
+
+# -- All changes are below here --
+# Build the databasefile array 
+
+$sourcedbfiles = Get-RubrikDatabaseFiles -id $SourceDBID -RecoveryDateTime $recoverdate
+$targetdbfiles = @()
+foreach ($i in $sourcedbfiles) {
+    $o = new-object psobject
+    $o | add-member -name logicalName -type noteproperty -value $i.logicalName
+    if ($dbfilepath) {
+        if ($i.originalName -notlike "*.ldf") {$o | add-member -name exportPath -type noteproperty -value $dbfilepath}
+        if ($i.originalName -like "*.ldf") {$o | add-member -name exportPath -type noteproperty -value $logfilepath}
+    } else {
+        $o | add-member -name exportPath -type noteproperty -value $i.originalPath
+    }
+    $targetdbfiles  += $o
+}
+
+$exportrequest = Export-RubrikDatabase -TargetFilePaths $targetdbfiles -id $SourceDBID -targetInstanceId $TargetInstanceID -targetDatabaseName $targetdbname -RecoveryDateTime $recoverdate -MaxDataStreams 4 -confirm:0 -finishrecovery
+
+$exportrequest.id  | out-file .\export.log
+# Get-RubrikRequest -id $exportrequest.id -type mssql
