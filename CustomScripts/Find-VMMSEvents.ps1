3b4dd00c35a16f6db09aa21549192e2e6680c538
J.R. Phillips <jr@pipefl.com>
2019-03-15 14:58:48 -0700

    Update Find-VMMSEvents.ps1

--- CustomScripts/Find-VMMSEvents.ps1
+++ CustomScripts/Find-VMMSEvents.ps1
@@ -13,6 +13,7 @@ param (
     .SYNOPSIS
     Generate CSV files for VMMS events from all nodes in the specified cluster
 
+
     .EXAMPLE
     Find-VMMSEvents.ps1 -start "3/11/2019 3:00 AM" -end "3/14/2019 5:00 PM" -cluster amer2-hvc -vmname app01
     This will return a report for each node name as a CSV with the VMMS events inside the specified date range. 
@@ -20,10 +21,15 @@ param (
 #>
 
 $clusternodes = (get-clusternode -Cluster $clustername).name
-$options = @{LogName ="Microsoft-Windows-Hyper-V-VMMS*"; StartTime = (get-date $start); EndTime = (get-date $end)}
+
+$options = @{}
+$options.add('LogName','Microsoft-Windows-Hyper-V-VMMS*')
+$options.add('StartTime',(get-date $start))
+$options.add('EndTime',(get-date $end))
 
 if ($clusternodes) {
     foreach ($i in $clusternodes) {
+        Write-Host -ForegroundColor green --- Checking VMMS events on $i ---`n
         $events = Get-WinEvent -FilterHashTable $options -ComputerName $i
         $filename = $i + '.csv'
         if ($vmname) {$events = $events | where-object {$_.message -match $vmname}}
